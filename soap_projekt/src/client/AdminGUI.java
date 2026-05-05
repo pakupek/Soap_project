0b58539f2236d1ada88acb517902aba51fc4ba55
pakupek <adam.akimowicz13@onet.eu>
2026-05-05 20:43:26 +0200

    dodanie dockera

--- soap_projekt/src/client/AdminGUI.java
+++ soap_projekt/src/client/AdminGUI.java
@@ -164,7 +164,7 @@ public class AdminGUI extends JFrame {
 
     private void initSOAP() {
         try {
-            URL url = new URL("http://100.64.218.17:8080/repair?wsdl");
+            URL url = new URL("http://192.168.1.107:8080/repair?wsdl");
             QName qname = new QName("http://server/", "RepairServiceImplService");
 
             Service s = Service.create(url, qname);
--- soap_projekt/src/server/ServerMain.java
+++ soap_projekt/src/server/ServerMain.java
@@ -3,7 +3,7 @@ import javax.xml.ws.Endpoint;
 
 public class ServerMain {
     public static void main(String[] args) {
-        Endpoint.publish("http://100.64.218.17:8080/repair", new RepairServiceImpl());
-        System.out.println("SOAP SERVER RUNNING: http://100.64.218.17:8080/repair?wsdl");
+        Endpoint.publish("http://0.0.0.0:8080/repair", new RepairServiceImpl());
+        System.out.println("SOAP SERVER RUNNING: http://0.0.0.0:8080/repair?wsdl");
     }
 }
